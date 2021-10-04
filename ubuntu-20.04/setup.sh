b25f9014a92ac6222607b4d8e7b08a559577dc1b
Josh Lowrance <38927314+lowranceworks@users.noreply.github.com>
2021-10-04 10:34:47 -0500

    incomplete zorin setup script

--- ubuntu-20.04/setup.sh
+++ ubuntu-20.04/setup.sh
@@ -1,1 +1,64 @@
-#!/bin/sh/
+ zorin os setup
+
+apt get update -y
+apt get upgrade -y
+
+# install basics
+apt install git vim wget -
+
+# install zorin windows support
+apt install zorin-windows-app-support -y
+
+# install and set zsh as default shell
+apt install zsh -y
+chsh -s /usr/bin/zsh root
+chsh -s /usr/bin/zsh josh # log out and log back to see zsh
+
+# install oh_my_zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+# powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+# open and install FireMono Nerd font
+wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraMono/Regular/complete/Fira%20Mono%20Regular%20Nerd%20Font%20Complete.otf?raw=true
+    # edit zsh theme : ~/.zshrc : ZSH_THEME="powerlevel10k/powerlevel10k"
+
+# install hashicorp tools
+apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt-get update && sudo apt-get install terraform packer
+
+# install docker
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io -y
+
+# install go
+cd /usr/local/
+## download go https://golang.org/dl/go1.17.1.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.1.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+# install partition manager
+apt install gparter -y
+
+# install extra gnome features
+apt install gnome-tweaks -y
+
+# install go
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.1.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
+
+# install backup manager
+apt install timeshift -y
+
+# config vscode
+
